d4a2a0aa17ccff5ad80632a4ea3752e85fdbe008
tuomt <46566906+tuomt@users.noreply.github.com>
2020-02-17 21:52:44 +0200

    Overhaul the module ship.py
    - Remove importing of os module
    - Change module to only import transform module from pygame
    - Add importing of GridComponent class
    - Remove class PositionError as it's not used anymore
    - Move class TransformError to gridcomponent.py
    - Remove unused classes:
      - Carrier
      - Battleship
      - Cruiser
      - Submarine
      - PatrolBoat
    
    Changes to the Ship class:
    
    - Change self._angles from list to tuple
    - Rename self._angle to self.rotated and change it's type to bool
    - Remove inheritance from pygame.sprite.Sprite
    - Add inheritance from GridComponent class
    - Remove the find_squares_group() method because it's not used
    - Move following methods to the GridComponent class:
      - transform_squares()
      - update_squares()
      - get_squares()
      - move_to()
      - move_up()
      - move_down()
      - move_right()
      - move_left()
      - draw()
    - Copy the scale_image() and load_image() methods to the GridComponent
      class with slight differences
    - Change the load_image() method to not update self.rect anymore because
      it's updated in the base classes method
    - Change the scale_image() and load_image() methods to override base
      class implementation
    - Change the rotate() method to update squares after rotation
    - Add documentation for the class and the methods

--- ship.py
+++ ship.py
@@ -1,177 +1,103 @@
-import pygame
-import os
+from pygame import transform
+from gridcomponent import GridComponent
 
-# When raising these exceptions, provide a message as an argument
-class PositionError(Exception):
-    pass
+class Ship(GridComponent):
+    """
+    A subclass of GridComponent for the warships to be placed on the game grid.
 
-class TransformError(Exception):
-    pass
+    :attr int self.size: how many squares the ship takes on the grid
+    :attr tuple self._angles: the two angles the ship can be rotated to
+    :attr bool self.rotated: is the ship rotated
+    :attr pygame.Surface self._default_surf: the non-rotated surface
+    :attr pygame.Surface self._rotated_surf: the rotated surface
+    :parent: gridcomponent.GridComponent
+    """
+    def __init__(self, size, square_size, grid_rect, square_group, image_name=None):
+        """
+        The constructor.
 
-class Ship(pygame.sprite.Sprite):
-    def __init__(self, size, square_size, image_name=None):
-        pygame.sprite.Sprite.__init__(self)
-
-        # Initialize variables
-        self._angles = [0, 90]
-        self._angle = 0
-        if image_name == None:
-            self.image_name = 'default.png'
-        else:
-            self.image_name = image_name
-        self._square_size = square_size
-        # How much space (squares) the ship takes on the gamegrid
+        :param int size: how many squares the ship takes on the grid
+        :param tuple square_size: the size (width, height) of a square on the grid
+        :param pygame.Rect grid_rect: the rectangle of the grid's boundaries
+        :param pygame.sprite.Group() square_group: the squares of the grid
+        :param str image_name: the filename of the image with the file extension included
+        """
         self.size = size
-        # Square(s) that the ship is placed on. Updated when the ship is placed successfully
-        self.__squares = None
-        # Load image to surfaces
+        self._angles = (0, 90)
+        self.rotated = False
+
         self._default_surf = None
         self._rotated_surf = None
-        self.rect = None
-        self.load_image(self.image_name)
-        # Scale the image size according to the square size
-        self.scale_image(self._square_size)
 
-    def transform_squares(self, square_size, new_squares):
-        # Get the position of the ship's first square
-        ship_pos = self.get_squares()[0].pos
-        # Scale the ship's image to fit the new squares
-        self.scale_image(square_size)
-        # Loop through the new squares and try to find the corresponding square
-        square_found = False
-        for square in new_squares:
-            if square.pos == ship_pos:
-                square_found = True
-                self.move_to(square.rect.x, square.rect.y)
-        if square_found == False:
-            message = "Corresponding square in new_squares was not found."
-            raise TransformError(message)
-        
-    def update_squares(self, square_group):
-        self.__squares = pygame.sprite.spritecollide(self, square_group, False)
-
-    def get_squares(self):
-        # Find the squares that the ship occupies currently
-        # returns list
-        if self.__squares == None:
-            message = "Ship has not been placed yet."
-            raise PositionError(message)
+        if image_name == None:
+            img = 'default.png'
         else:
-            return self.__squares
+            img = image_name
 
-    def move_to(self, x, y):
-        self.rect.x = x
-        self.rect.y = y
-    
-    def move_up(self, pixels, bounds = None):
-        if bounds != None:
-            rect_copy = self.rect.copy()
-            rect_copy.move_ip(0, -pixels)
-            if bounds.contains(rect_copy):
-                self.rect.move_ip(0, -pixels)
-                return True
-            else:
-                return False
-        else:
-            self.rect.move_ip(0, -pixels)
-            return True
+        GridComponent.__init__(self, square_size, grid_rect, square_group, img)
+                
+    def rotate(self, bounds=None):
+        """
+        Rotate the ship if it's in bounds.
 
-    def move_down(self, pixels, bounds = None):
-        if bounds != None:
-            rect_copy = self.rect.copy()
-            rect_copy.move_ip(0, pixels)
-            if bounds.contains(rect_copy):
-                self.rect.move_ip(0, pixels)
-                return True
-            else:
-                return False
-        else:
-            self.rect.move_ip(0, pixels)
-            return True
-
-    def move_right(self, pixels, bounds = None):
-        if bounds != None:
-            rect_copy = self.rect.copy()
-            rect_copy.move_ip(pixels, 0)
-            if bounds.contains(rect_copy):
-                self.rect.move_ip(pixels, 0)
-                return True
-            else:
-                return False
-        else:
-            self.rect.move_ip(pixels, 0)
-            return True
-
-    def move_left(self, pixels, bounds = None):
-        if bounds != None:
-            rect_copy = self.rect.copy()
-            rect_copy.move_ip(-pixels, 0)
-            if bounds.contains(rect_copy):
-                self.rect.move_ip(-pixels, 0)
-                return True
-            else:
-                return False
-        else:
-            self.rect.move_ip(-pixels, 0)
-            return True
-
-    def rotate(self, bounds = None):
+        :param pygame.Rect bounds: the boundaries of the grid on which the ship is placed on
+        :return: True if the ship was rotated, False if the new position was out of bounds
+        :rtype: bool
+        """
         # A test rectangle is created first to check if the rotated ship is inside the boundaries
         # If the ship cannot be rotated, the function will return False
-        if self._angle == self._angles[0]:
-            test_rect = self._rotated_surf.get_rect()
-        else:
+        if self.rotated:
             test_rect = self._default_surf.get_rect()
+        else:
+            test_rect = self._rotated_surf.get_rect()
         test_rect.center = self.rect.center
         if self.size % 2 == 0:
-            if self._angle == self._angles[0]:
-                test_rect.x -= self._square_size[0] / 2
-                test_rect.y += self._square_size[1] / 2
+            if self.rotated:
+                test_rect.x += self.square_size[0] / 2
+                test_rect.y -= self.square_size[1] / 2
             else:
-                test_rect.x += self._square_size[0] / 2
-                test_rect.y -= self._square_size[1] / 2
+                test_rect.x -= self.square_size[0] / 2
+                test_rect.y += self.square_size[1] / 2
 
         if bounds.contains(test_rect):
             # The test rect is in bounds so it is assigned to the ship's rect property
             self.rect = test_rect
             # Update the current angle and image surface
-            if self._angle == self._angles[0]:
-                self._angle = self._angles[1]
-                self.image = self._rotated_surf
-            else:
-                self._angle = self._angles[0]
+            if self.rotated:
                 self.image = self._default_surf
+                self.rotated = False
+            else:
+                self.image = self._rotated_surf
+                self.rotated = True
+            self.update_squares()
             return True
         else:
             return False
 
-    def find_squares_group(self, square_group):
-        # Find the squares that the ship occupies currently
-        # returns pygame.sprite.Group() object
-        square_list = pygame.sprite.spritecollide(self, square_group, False)
-        squares = pygame.sprite.Group()
-        for square in square_list:
-            squares.add(square)
-        return squares
-
     def scale_image(self, size):
-        # Scales the image based on the size argument
-        # Then assigns the scaled image to the surface attributes
-        # and updates the rect attribute
-        # Returns the updated image surface
+        """
+        Scale the image according to the size parameter.
+        Then assing the scaled image to the self.image attribute.
+        Takes in account the rotation of the ship.
+        Update the self.rect attribute.
+        Overrides the method of the base class.
 
+        :param tuple size: the size (width, height) the image is scaled to
+        :return: the scaled image
+        :rtype: pygame.Surface
+        """
         # Scale
         width = size[0]
         height = size[1]
-        self._default_surf = pygame.transform.scale(self._default_surf, (width * self.size, height))
-        self._rotated_surf = pygame.transform.scale(self._rotated_surf, (width, height * self.size))
+        self._default_surf = transform.scale(self._default_surf, (width * self.size, height))
+        self._rotated_surf = transform.scale(self._rotated_surf, (width, height * self.size))
         rotated = self._angles[1]
-        if self._angle == rotated:
+        if self.rotated:
             self.image = self._rotated_surf.copy()
         else:
             self.image = self._default_surf.copy()
 
-        # Update the rectangle of the ship
+        # Update the rectangle
         current_x = self.rect.x
         current_y = self.rect.y
         self.rect = self.image.get_rect()
@@ -180,40 +106,19 @@ class Ship(pygame.sprite.Sprite):
         return self.image
 
     def load_image(self, image_name):
-        # Loads the image
-        # Returns the updated image surface
-        self.image = pygame.image.load(os.path.join('data', image_name)).convert_alpha() 
-        self.image_name = image_name
+        """
+        Load an image to the self.image surface by calling base class load_image method.
+        Set the default and rotated surfaces.
+        Overrides the method of the base class.
+
+        :param str image_name: the filename of the image with the file extension included
+        :return: the loaded image
+        :rtype: pygame.Surface
+        """
+        self.image = GridComponent.load_image(self, image_name)
         # Set the default surface
         self._default_surf = self.image.copy()
         # Set the rotated surface
-        self._rotated_surf = pygame.transform.rotate(self.image.copy(), self._angles[1])
-        # Update the rectangle of the ship
-        self.rect = self.image.get_rect()
+        self._rotated_surf = transform.rotate(self.image.copy(), self._angles[1])
 
         return self.image
-
-    def draw(self, surface):
-        surface.blit(self.image, (self.rect.x, self.rect.y))
-
-class Carrier(Ship):
-    def __init__(self, size, square_size):
-        Ship.__init__(self, size, square_size)
-
-class Battleship(Ship):
-    def __init__(self, size, square_size):
-        Ship.__init__(self, size, square_size)
-
-class Cruiser(Ship):
-    def __init__(self, size, square_size):
-        Ship.__init__(self, size, square_size)
-
-class Submarine(Ship):
-    def __init__(self, size, square_size):
-        Ship.__init__(self, size, square_size)
-
-class PatrolBoat(Ship):
-    def __init__(self, size, square_size):
-        Ship.__init__(self, size, square_size)
-
-
