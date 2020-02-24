885a43d4613d605a7fb9fc4695aa9188427e2897
tuomt <46566906+tuomt@users.noreply.github.com>
2020-02-24 23:17:35 +0200

    Add attribute for Settings class
    self.total_reserved_squares defines the sum of squares that the
    ships occupy on the grid.

--- settings.py
+++ settings.py
@@ -7,9 +7,9 @@ class Settings:
         self.columns = 10
         # Ship counts
         self.carrier_count = 1
-        self.battleship_count = 1
+        self.battleship_count = 0
         self.cruiser_count = 0
-        self.submarine_count = 0
+        self.submarine_count = 1
         self.patrol_boat_count = 0
         self.ship_count = self.carrier_count + self.battleship_count + self.cruiser_count + self.submarine_count + self.patrol_boat_count
 
@@ -19,3 +19,11 @@ class Settings:
         self.cruiser_size = 3
         self.submarine_size = 2
         self.patrol_boat_size = 1
+
+        # Total amount of squares that all ships occupy on the grid
+        self.total_reserved_squares = \
+                self.carrier_count * self.carrier_size \
+                + self.battleship_count * self.battleship_size \
+                + self.cruiser_count * self.cruiser_size \
+                + self.submarine_count * self.submarine_size \
+                + self.patrol_boat_count * self.patrol_boat_size
